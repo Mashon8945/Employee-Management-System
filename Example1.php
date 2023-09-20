45c5380626f6ae37bef7cfda5dec52247cfb1026
Le'Mashon <80026977+Mashon8945@users.noreply.github.com>
2023-09-20 12:36:35 +0300

    Update Example1.php

--- Example1.php
+++ Example1.php
@@ -1,11 +1,1 @@
-<?php
- 	function MaxNumbers($number){ 
-		rsort($number); //descending order
-		//$highestNumber = array_slice($number, 0,2);
-		$product = $number[0] * $number[1];
-		return $product;
- 	}
-	$number = array('1', '2', '3', '4', '5', '6', '7', '8', '9');
-	$result = MaxNumbers($number);
-	echo $result;
-?>
+
